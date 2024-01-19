ddc6b3f42389ac989f3106833b7b2679f413bbd0
glenn guan <glenn.profile@gmail.com>
2024-01-19 17:41:56 +0800

    chore(TDD): added

---
+++ TDD/App/tests/Unit/CalculatorTest.php
@@ -0,0 +1,101 @@
+<?php
+/*
+計算出手牌(hand)的點數(point)
+
+點數計算規則
+2-10 的點數(point)為牌面數字
+J、Q、K 每張為 10 點
+A 可以是 1 點或 11 點,取决於哪個值能讓手中牌點數最接近 21 點但不超過。
+----
+
+OCP
+Calculator
+Poker-Dictionary
+發牌 -> 我手上的牌
+計算所有的可能, 要小於等於 21, 不能超過
+超過也要計算, 超過時 A 為 1
+"A,10,3"
+
+*/
+
+class HandCards
+{
+    public function calculator(array $cards): int
+    {
+        $resultPoint = 0;
+        $expectAArray = [];
+        $Acards = [];
+        foreach ($cards as $card) {
+            if ($card == 'A') {
+                $Acards [] = $card;
+            } else {
+                $expectAArray[] = $card;
+            }
+        }
+
+        foreach ($expectAArray as $card) {
+
+            switch ($card) {
+                case 'J':
+                case 'Q':
+                case 'K':
+                    $resultPoint += 10;
+                    break;
+                default:
+                    $resultPoint += $card;
+            }
+        }
+
+        foreach ($Acards as $card) {
+
+            if ($resultPoint > 21 || count($cards) > 2) {
+                $resultPoint += 1;
+            } else {
+                $resultPoint += 11;
+            }
+        }
+        return $resultPoint;
+    }
+}
+
+
+it('can calculator poker point', function () {
+    $handCards = new HandCards();
+    $points = $handCards->calculator([2, 3, 4]);
+    expect($points)->toBe(9);
+});
+it('can calculator J Q K', function () {
+    $handCards = new HandCards();
+    $points = $handCards->calculator(['J', 'Q', 'K']);
+    expect($points)->toBe(30);
+});
+it('can calculator A Q', function () {
+    $handCards = new HandCards();
+    $points = $handCards->calculator(['A', 'Q']);
+    expect($points)->toBe(21);
+});
+it('can calculator A J', function () {
+    $handCards = new HandCards();
+    $points = $handCards->calculator(['A', 'J']);
+    expect($points)->toBe(21);
+});
+it('can calculator A K', function () {
+    $handCards = new HandCards();
+    $points = $handCards->calculator(['A', 'K']);
+    expect($points)->toBe(21);
+});
+it('can calculator A K K', function () {
+    $handCards = new HandCards();
+    $points = $handCards->calculator(['A', 'K', 'K']);
+    expect($points)->toBe(21);
+});
+it('can calculator A A K', function () {
+    $handCards = new HandCards();
+    $points = $handCards->calculator(['A', 'A', 'K']);
+    expect($points)->toBe(12);
+});
+it('can calculator A 2 2', function () {
+    $handCards = new HandCards();
+    $points = $handCards->calculator(['A', '2', '2']);
+    expect($points)->toBe(15);
+});
